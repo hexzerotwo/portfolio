c881708f85e06bdc020ba830b03ee60301cb8dcc
hexzerotwo <162190960+hexzerotwo@users.noreply.github.com>
2025-09-24 10:38:53 +0800

    Update send_email.php

--- send_email.php
+++ send_email.php
@@ -1,15 +1,16 @@
-<form action="https://formspree.io/f/mldpeaek" method="POST">
-  <label for="name">Name</label>
-  <input type="text" name="name" required>
-
-  <label for="email">Email</label>
-  <input type="email" name="email" required>
-
-  <label for="subject">Subject</label>
-  <input type="text" name="subject">
-
-  <label for="message">Message</label>
-  <textarea name="message" required></textarea>
-
+<!-- modify this form HTML and place wherever you want your form -->
+<form
+  action="https://formspree.io/f/mldpeaek"
+  method="POST"
+>
+  <label>
+    Your email:
+    <input type="email" name="email">
+  </label>
+  <label>
+    Your message:
+    <textarea name="message"></textarea>
+  </label>
+  <!-- your other form fields go here -->
   <button type="submit">Send</button>
 </form>
