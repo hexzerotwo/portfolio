f05ad45677cb130fa01a68160bdd9ba0b3ed3cc6
hexzerotwo <162190960+hexzerotwo@users.noreply.github.com>
2025-09-24 10:37:34 +0800

    Update send_email.php with Formspree.io

--- send_email.php
+++ send_email.php
@@ -1,26 +1,15 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST["name"];
-    $email = $_POST["email"];
-    $subject = $_POST["subject"];
-    $message = $_POST["message"];
+<form action="https://formspree.io/f/mldpeaek" method="POST">
+  <label for="name">Name</label>
+  <input type="text" name="name" required>
 
-    // Construct the email message
-    $to = "johnrichardlagbas@gmail.com";
-    $headers = "From: $name <$email>";
-    $email_body = "Name: $name\n";
-    $email_body .= "Email: $email\n";
-    $email_body .= "Subject: $subject\n";
-    $email_body .= "Message:\n$message";
+  <label for="email">Email</label>
+  <input type="email" name="email" required>
 
-    // Send the email
-    if (mail($to, $subject, $email_body, $headers)) {
-        echo "Your message has been sent successfully.";
-    } else {
-        echo "Sorry, there was an error sending your message.";
-    }
-} else {
-    // If the request method is not POST, redirect to the homepage or display an error message
-    echo "Error: Invalid request method.";
-}
-?>
+  <label for="subject">Subject</label>
+  <input type="text" name="subject">
+
+  <label for="message">Message</label>
+  <textarea name="message" required></textarea>
+
+  <button type="submit">Send</button>
+</form>
